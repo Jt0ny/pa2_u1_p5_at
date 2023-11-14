428e9f40d53824892bf39142b26f2392107b1f28
Anthony T <jtonytipan@gmail.com>
2023-11-13 19:40:51 -0500

    Taller 5

--- src/main/java/com/uce/edu/Pa2U1P5AtApplication.java
+++ src/main/java/com/uce/edu/Pa2U1P5AtApplication.java
@@ -1,23 +1,32 @@
 package com.uce.edu;
 
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.uce.edu.ioc.di.Estudiante;
+
 @SpringBootApplication
 public class Pa2U1P5AtApplication implements CommandLineRunner{
-
+	
+	@Autowired
+	private Estudiante estudiante;
+	
 	public static void main(String[] args)  {
 		SpringApplication.run(Pa2U1P5AtApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hola mundo");
-		System.out.println("Soy Anthony Tipan");
-		System.out.println("Cambio en la misma rama");
-		System.out.println("Cambio en una nueva rama taller 3");
-		System.out.println("Cambio TALLER 4 version 2");
+		this.estudiante.setApellido("Tipan");
+		this.estudiante.setNombre("Anthony");
+		this.estudiante.setCedula("123465");
+		this.estudiante.setSalario(new BigDecimal(400));
+		System.out.println(estudiante);
+		
 		
 		
 	}
